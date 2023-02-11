6ca055252a1d80b376e67b50c2908d3977b48d6d
Abhay Goyal <91728146+AbhayGoyal001@users.noreply.github.com>
2023-02-11 15:35:07 +0530

    Add files via upload

---
+++ Recursion/Combination_Sum.java
@@ -0,0 +1,18 @@
+package Recursion;
+
+import java.util.ArrayList;
+
+public class Combination_Sum {
+    public static void main(String[] args) {
+        int [] arr = {2,3,6,7};
+        int target = 7;
+        ArrayList<Integer> list = new ArrayList<Integer>();
+    }
+    public static void combination(int [] arr , int target , int i,ArrayList<Integer> list){
+        if(i==arr.length-1){
+            if(target==0){
+                System.out.println();
+            }
+        }
+    }
+}
